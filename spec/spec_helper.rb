df12648b2584b44732d6d3d28470f1bfec071822
David Tengdin <dreamingfurther@gmail.com>
2015-09-16 13:54:02 -0400

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,4 @@ require "rspec"
 require_relative "../lib/board"
 require_relative "../lib/game"
 require_relative "../lib/player"
+# my biggest suggestion here if you wanted to really get through with your tests would be to figure out some way to full test playing the entire game. You'd need to mock/stub the actual interface to stout so that is certainly much more compliced. Otherwise these are good fine grained unit level tests!
